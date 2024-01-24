dd8cdd4c5b16b65ff76c410e357b694ec1797d76
Z-HUANT <1781898481@qq.com>
2024-01-24 21:28:25 +0800

    fix: 回退 GraphTransaction.java

--- hugegraph-server/hugegraph-core/src/main/java/org/apache/hugegraph/backend/tx/GraphTransaction.java
+++ hugegraph-server/hugegraph-core/src/main/java/org/apache/hugegraph/backend/tx/GraphTransaction.java
@@ -29,7 +29,6 @@ import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.hugegraph.HugeException;
@@ -1451,52 +1450,25 @@ public class GraphTransaction extends IndexableTransaction {
         // Optimize edge query
         if (query.resultType().isEdge() && label != null &&
             query.condition(HugeKeys.OWNER_VERTEX) != null &&
-            query.condition(HugeKeys.DIRECTION) != null) {
+            query.condition(HugeKeys.DIRECTION) != null &&
+            matchEdgeSortKeys(query, false, this.graph())) {
+            // Query edge by sourceVertex + direction + label + sort-values
+            query.optimized(OptimizedType.SORT_KEYS);
+            query = query.copy();
+            // Serialize sort-values
+            List<Id> keys = this.graph().edgeLabel(label).sortKeys();
+            List<Condition> conditions =
+                            GraphIndexTransaction.constructShardConditions(
+                            query, keys, HugeKeys.SORT_VALUES);
+            query.query(conditions);
+            /*
+             * Reset all userprop since transferred to sort-keys, ignore other
+             * userprop(if exists) that it will be filtered by queryEdges(Query)
+             */
+            query.resetUserpropConditions();
 
-            Directions dir = query.condition(HugeKeys.DIRECTION);
-            EdgeLabel edgeLabel = this.graph().edgeLabel(label);
-
-            LOG.info("Optimize edge query start [{}]", query);
-            if (query.containsRelation(HugeKeys.OWNER_VERTEX, Condition.RelationType.IN)) {
-                // For IN queries, filter out non-adjacent vertices.
-                ArrayList<Id> vertexIdList = query.condition(HugeKeys.OWNER_VERTEX);
-                List<Id> filterVertexList = vertexIdList.stream().filter(vertexId -> {
-                    Vertex vertex = this.graph().vertex(vertexId);
-                    VertexLabel vertexLabel = graph().vertexLabel(vertex.label());
-                    return edgeLabel.linkWithLabel(vertexLabel.id(), dir.type());
-                }).collect(Collectors.toList());
-                vertexIdList.clear();
-                vertexIdList.addAll(filterVertexList);
-            } else if (query.containsRelation(HugeKeys.OWNER_VERTEX, Condition.RelationType.EQ)) {
-                // For EQ queries, return emptyQuery() if it is a non-adjacent vertex.
-                Id vertexId = query.condition(HugeKeys.OWNER_VERTEX);
-                Vertex vertex = this.graph().vertex(vertexId);
-                VertexLabel vertexLabel = graph().vertexLabel(vertex.label());
-                if (!edgeLabel.linkWithLabel(vertexLabel.id(), dir.type())) {
-                    return new Query(query.resultType());
-                }
-            }
-            LOG.info("Optimize edge query end [{}]", query);
-
-            if (matchEdgeSortKeys(query, false, this.graph())) {
-                // Query edge by sourceVertex + direction + label + sort-values
-                query.optimized(OptimizedType.SORT_KEYS);
-                query = query.copy();
-                // Serialize sort-values
-                List<Id> keys = this.graph().edgeLabel(label).sortKeys();
-                List<Condition> conditions =
-                    GraphIndexTransaction.constructShardConditions(
-                        query, keys, HugeKeys.SORT_VALUES);
-                query.query(conditions);
-                /*
-                 * Reset all userprop since transferred to sort-keys, ignore other
-                 * userprop(if exists) that it will be filtered by queryEdges(Query)
-                 */
-                query.resetUserpropConditions();
-
-                LOG.debug("Query edges by sortKeys: {}", query);
-                return query;
-            }
+            LOG.debug("Query edges by sortKeys: {}", query);
+            return query;
         }
 
         /*
